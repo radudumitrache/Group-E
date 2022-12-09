2b4e217641589aba83798518d7ed1396969c117b
NefeliChrysostomou <nefelichrysostomou@student.nhlstenden.com>
2022-12-09 21:46:46 +0100

    Finished Styling Table and Surrounding Box

--- root/parentScores-page.php
+++ root/parentScores-page.php
@@ -40,55 +40,68 @@
 
 </header>
 
-<main>
+	<main>
 		<div class="MainBox">
-			<table class="table">
-				<tr>
-					<th>Course</th>
-					<th>Score</th>
-					<th id="NotesTh">Notes</th>
-				</tr>
-				<tr>
-					<td class="RightTds">fvgbhnjmk</td>
-					<td></td>
-					<td class="LeftTds"></td>
-				</tr>
-				<tr>
-					<td class="RightTds">yo</td>
-					<td></td>
-					<td class="LeftTds"></td>
-				</tr>
-				<tr>
-					<td class="RightTds">aye</td>
-					<td></td>
-					<td class="LeftTds"></td>
-				</tr>
-				<tr>
-					<td class="RightTds">bruh</td>
-					<td></td>
-					<td class="LeftTds"></td>
-				</tr>
-				<tr>
-					<td class="RightTds">blah</td>
-					<td></td>
-					<td class="LeftTds"></td>
-				</tr>
-				<tr>
-					<td class="RightTds">ue</td>
-					<td></td>
-					<td class="LeftTds"></td>
-				</tr>
-				<tr>
-					<td class="RightTds">eh</td>
-					<td></td>
-					<td class="LeftTds"></td>
-				</tr>
-				<tr>
-					<td class="RightTds" id="BottomRow">man</td>
-					<td id="BottomRow"></td>
-					<td class="LeftTds" id="BottomRow"></td>
-				</tr>
-			</table>
+			<div class="AboveContent">
+				<a class="button1" href="parentProfile.php">
+          			<button>Profile</button>
+        		</a>
+				<h1 class="TextHeading">Scores</h1>
+				<img
+					src="img/studentImg.svg" 
+					title="studentImg" 
+					alt="Image of a Student"
+				>
+			</div>
+			<div class="TableAlign">
+				<table class="Table">
+					<tr>
+						<th>Course</th>
+						<th>Score</th>
+						<th id="NotesTh">Notes</th>
+					</tr>
+					<tr>
+						<td class="RightTds">fvgbhnjmk</td>
+						<td></td>
+						<td class="LeftTds"></td>
+					</tr>
+					<tr>
+						<td class="RightTds">yo</td>
+						<td></td>
+						<td class="LeftTds"></td>
+					</tr>
+					<tr>
+						<td class="RightTds">aye</td>
+						<td></td>
+						<td class="LeftTds"></td>
+					</tr>
+					<tr>
+						<td class="RightTds">bruh</td>
+						<td></td>
+						<td class="LeftTds"></td>
+					</tr>
+					<tr>
+						<td class="RightTds">blah</td>
+						<td></td>
+						<td class="LeftTds"></td>
+					</tr>
+					<tr>
+						<td class="RightTds">ue</td>
+						<td></td>
+						<td class="LeftTds"></td>
+					</tr>
+					<tr>
+						<td class="RightTds">eh</td>
+						<td></td>
+						<td class="LeftTds"></td>
+					</tr>
+					<tr>
+						<td class="RightTds" id="BottomRow">man</td>
+						<td id="BottomRow"></td>
+						<td class="LeftTds" id="BottomRow"></td>
+					</tr>
+				</table>
+			</div>
 		</div>
 	</main>
 
