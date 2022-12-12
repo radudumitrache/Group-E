003867be1846f56126e571782903d30ea8f5382e
Vlad Chitac <vlad.chitac@student.nhlstenden.com>
2022-12-12 12:26:15 +0100

    added basis part of front-end

--- root/teacherAbsentces-page.php
+++ root/teacherAbsentces-page.php
@@ -9,41 +9,37 @@
   <meta name="keywords" content="Morgenster, school, children, learn, study, Emmen, Netherlands">
   <meta name="author" content="Yourname">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
   <title>Morgenster</title>
-  <link rel="stylesheet" href="css/teacherAbsentec-page.css">
+  <link rel="stylesheet" href="css/teacherAbsencespage.css">
   <link rel="icon" href="img/logo.svg">
 
 </head>
 
 <body>
 
-<header>
+  <div id="container">
+    
+    <header>
 
-  <a class="logo" href="home-page.php">
-    <img src="img/logo.svg" alt="logo">
-  </a>
-
-  <div class="listcontainer">
-
-    <ul class="headerList">
-
-      <li>
-        <a class="button1" href="home-page.php">
-          <button>Sign out</button>
+      <div>
+        <a class="logo" href="home-page.php">
+          <img src="img/logo.svg" alt="logo">
         </a>
-      </li>
+      </div>
 
-    </ul>
+      <div class="listContainer">
+        <ul class="headerList">
+          <li>
+            <a class="button1" href="home-page.php">
+              <button>SignOut</button>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+    </header>
 
   </div>
-
-</header>
-
-
-
-
-
 
 </body>
 </html>
