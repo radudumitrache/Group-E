7e454d41ac331ab191713cf96f2ed04c543972db
AlikhanSmagulov <alikhan.smagulov@student.nhlstenden.com>
2022-12-16 12:11:09 +0100

    added parent profile page, styles and images for it

--- root/parentProfile-page.php
+++ root/parentProfile-page.php
@@ -4,15 +4,15 @@
 
 <head>
 
-  <meta charset="UTF-8">
-  <meta name="description" content="Are you worry about your children future? Register at our school and we show them the future">
-  <meta name="keywords" content="Morgenster, school, children, learn, study, Emmen, Netherlands">
-  <meta name="author" content="Yourname">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="description" content="Are you worry about your children future? Register at our school and we show them the future">
+    <meta name="keywords" content="Morgenster, school, children, learn, study, Emmen, Netherlands">
+    <meta name="author" content="Yourname">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title>Morgenster</title>
-  <link rel="stylesheet" href="css/parentProfile-page.css">
-  <link rel="icon" href="img/logo.svg">
+    <title>Morgenster</title>
+    <link rel="stylesheet" href="css/parentProfile-page.css">
+    <link rel="icon" href="img/logo.svg">
 
 </head>
 
@@ -20,27 +20,61 @@
 
 <header>
 
-  <a class="logo" href="home-page.php">
-    <img src="img/logo.svg" alt="logo">
-  </a>
+    <a class="logo" href="home-page.php">
+        <img src="img/logo.svg" alt="logo">
+    </a>
 
-  <div class="listcontainer">
+    <div class="listcontainer">
 
-    <ul class="headerList">
+        <ul class="headerList">
 
-      <li>
-        <a class="button1" href="home-page.php">
-          <button>Sign out</button>
-        </a>
-      </li>
+            <li>
+                <a class="button1" href="home-page.php">
+                    <button>Sign out</button>
+                </a>
+            </li>
 
-    </ul>
+        </ul>
 
-  </div>
+    </div>
 
 </header>
+<main>
+    <div class="border">
+        <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg" id="backButton">
+            <rect width="60" height="60" rx="15" fill="#FDDF42"/>
+            <line x1="38.7827" y1="14.3451" x2="18.4498" y2="30.2309" stroke="black" stroke-width="2" stroke-linecap="round"/>
+            <line x1="39.2916" y1="47.9034" x2="18.173" y2="31.4037" stroke="black" stroke-width="2" stroke-linecap="round"/>
+        </svg>
 
+        <h1>Profile</h1>
+        <img src="img/kid.jpg" id="ProfilePhoto">
+        <p>Change profile photo</p>
+        <h2>Radu</h2>
 
+        <div id="parents">
+            <div class="parent">
+                <a class="button2" href="parentProfile-page.php">
+                    <button>Parent1</button>
+                </a>
+                <h3>Phone Number: +3123456789</h3>
+                <h3>E-mail: example@gmail.com</h3>
+            </div>
+
+            <div class="parent">
+                <a class="button2" href="parentProfile-page.php">
+                    <button>Parent1</button>
+                </a>
+                <h3>Phone Number: +3123456789 </h3>
+                <h3>E-mail: example@gmail.com</h3>
+            </div>
+        </div>
+
+        <a class="button2" href="parentScores-page.php">
+            <button>Scores</button>
+        </a>
+    </div>
+</main>
 
 
 
