5fde3e3b7de3feac3e9e299d275c791c45e62f15
Francisco Ferioli <francisco.ferioli@student.nhlstenden.com>
2022-12-27 16:27:04 -0300

    I edited the PHP and CSS of the home page and the events pages. Now they are dynamic.

--- root/home-page.php
+++ root/home-page.php
@@ -18,63 +18,73 @@
 
 <body>
 
-<header>
+	<header>
 
-	<a class="logo" href="">
-		<img src="img/logo.svg" alt="logo">
-	</a>
+		<a class="logo" href="">
+			<img src="img/logo.svg" alt="logo">
+		</a>
 
-	<div class="listcontainer">
+		<div class="listcontainer">
 
-		<ul class="headerList">
+			<ul class="headerList">
 
-			<li>
-				<a class="button1" href="signIn-page.php">
-					<button>Sign in</button>
-				</a>
-			</li>
+				<li>
+					<a class="button1" href="signIn-page.php">
+						<button>Sign in</button>
+					</a>
+				</li>
 
-			<li>
-				<a class="button2" href="aboutUs-page.php">
-					<button>About us</button>
-				</a>
-			</li>
+				<li>
+					<a class="button2" href="aboutUs-page.php">
+						<button>About us</button>
+					</a>
+				</li>
 
-		</ul>
+			</ul>
 
-	</div>
+		</div>
 
-</header>
-
-<body>
+	</header>
 
 	<main>
 
-		<?php
+		<img src="../root/img/morgenster-naambord-1.png">
+		<p>Welcome to Christian primary school De Morgenster. The school is located in the Westenholte district of Zwolle. The Morning Star makes you shine; by looking at each individual child, we can best meet the needs of that student and there is room for his or her talents.</p>
 
-			$src = array("11", "12", "13");
-			$alt = array("Boy reading", "Boy writing", "Set of pencils");
-			$txt = array("Studying affects imagination", "Practical learning", "Creativity");
+		<div id="pages-container">
 
-			function generate_div($no) {
-				global $src;
-				global $alt;
-				global $txt;
-					echo
-						"<div id='pages'>
-							<img
-								src='../root/img/rectangle-{$src[$no]}.png'
-								alt='$alt[$no]'
-							>
-							<p>$txt[$no]</p>
-						</div>";
-			}
+			<?php
 
-			for ($x = 0; $x <= 2; $x++) {
-				echo generate_div($x);
-			}
+				$a_src = array("1", "2", "3");
+				$img_src = array("11", "12", "13");
+				$alt = array("Boy reading", "Boy writing", "Set of pencils");
+				$txt = array("Studying affects imagination", "Practical learning", "Creativity");
 
-		?>
+				function generate_div($no) {
+					global $a_src;
+					global $img_src;
+					global $alt;
+					global $txt;
+						echo
+							"<a href='../root/event{$a_src[$no]}-page.php'>
+								<div class='pages'>
+									<img
+										src='../root/img/rectangle-{$img_src[$no]}.png'
+										alt='$alt[$no]'
+									>
+									<p>$txt[$no]</p>
+									<span class='hovertext'>Read more</span>
+								</div>
+							</a>";
+				}
+
+				for ($x = 0; $x <= 2; $x++) {
+					echo generate_div($x);
+				}
+
+			?>
+
+		</div>
 
 	</main>
 
