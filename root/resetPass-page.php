a7b33aff2cd27b34b69de7cfee42c1242653c740
Vlad Chitac <vlad.chitac@student.nhlstenden.com>
2023-01-10 12:17:23 +0100

    added the basic form checks for reset password page

--- root/resetPass-page.php
+++ root/resetPass-page.php
@@ -1,7 +1,79 @@
+<?php
+
+  
+
+  $err = [];
+
+
+  $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+  $codeEnter = filter_input(INPUT_POST, 'fillCode', FILTER_VALIDATE_INT);
+  $password = filter_input(INPUT_POST, 'newPass', FILTER_SANITIZE_SPECIAL_CHARS);
+
+  if(!empty($password)) {
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+  }
+
+  if(isset($_POST["sendCode"])){
+
+    if(!empty($_POST['email'])){
+
+      if(empty($email)){
+        $err[] = "Invalid email<br>";
+      }else{
+        echo "Code has been sent";
+      }
+
+    }else{
+      $err[] = "Enter email";
+    }
+
+    if(count($err) > 0){
+      echo "<ul>";
+          foreach($err as $error){
+              echo "<li>$error</li>";
+          }
+      echo "</ul>";
+  }
+
+  }
+
+  if(isset($_POST["submit"])){
+
+    if(!empty($_POST['email'])){
+
+      if(empty($email)){
+        $err[] = "Invalid email<br>";
+      }
+      
+    }else{
+      $err[] = "Enter email";
+    }
+
+    if(empty($_POST['fillCode'])){
+      $err[] = "Code is missing";
+    }else{
+      if(empty($codeEnter)){
+        $err[] = "Invalid code<br>";
+      }
+    }
+    if(empty($_POST['newPass'])){
+      $err[] = "Enter a new password";
+    }
+
+    if(count($err) > 0){
+      echo "<ul>";
+          foreach($err as $error){
+              echo "<li>$error</li>";
+          }
+      echo "</ul>";
+    }
+
+  }
+
+?>
+
 <!DOCTYPE html>
-
 <html lang="en">
-
 <head>
 
   <meta charset="UTF-8">
@@ -46,48 +118,25 @@
         </a>
       </li>
 
-
     </ul>
 
   </div>
 </header>
   <main>
     <div id="formContainer">
-
-      <h1>Reset Password</h1>
-
-      <div class="forms">
-
-        <form action="<?php  htmlentities($_SERVER['PHP_SELF'])?>" method="post" class="form1">
-
-          <div class="input1">
-            <label for="email">Enter your E-mail</label>
-              <input type="email" id="email" name="email">
-          </div>
-
-          <div class="input1">
-            <label for="code">Enter the code</label>
-            <div class="input2">
-                <input type="text" id="code" name="code">
-                <input type="submit" id="submit1" name="sendCode" value="Send code">
-            </div>
-          </div>
-
-        </form>
-
-        <form action="<?php  htmlentities($_SERVER['PHP_SELF'])?>" method="post" class="form1">
-        <div>
-          <label for="newPass">New Password</label>
-          <div class="input2">
-            <input type="text" id="code" name="newPassword">
-            <input type="submit" id="submit1" name="submit" value="Submit">
-          </div>
+      <h1> Reset Password</h1>
+      <form id="codeForm" action="resetPass-page.php" method="post">  
+        <label for="email">Enter your E-mail</label>
+        <div id="codeInfo">
+          <input type="text" id="email" name="email" <?= isset($_POST["email"]) ? "value='" . $_POST["email"] . "'" : "" ?>>
+          <input type="submit" name="sendCode">  
         </div>
-
-        </form>
-
-      </div>
-
+        <label for="code">Enter code </label>
+        <input type="text" id="code" name="fillCode">
+        <label for="newPass">New Password</label>
+        <input type="text" id ="newPass" name="newPass"> 
+        <input type="submit" id="submit" name="submit">
+      </form>
     </div>
   </main>
 </body>
